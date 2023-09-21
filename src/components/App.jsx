2d6015774789a126f3c0eb70dc899154e4b4ccb2
Roman <jrj80mail@gmail.com>
2023-09-21 23:59:58 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,10 @@
-import { Profile } from './Profile/Profile';
 import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
+import { Profile } from './Profile/Profile';
+
 import css from './styles/Layout.module.css';
 
 export const App = () => {
