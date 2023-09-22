a44f0a4ff26fcc32a8a72f675113164c6611fab4
Roman <jrj80mail@gmail.com>
2023-09-22 22:32:45 +0300

    Create style `Statistics.module.css`

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,15 +1,20 @@
 import { getRandomColor } from './getRandomColor';
+import css from './Statistics.module.css';
 
 export const Statistics = ({ title, stats }) => {
   return (
-    <section class="statistics">
-      {title && <h2 class="title">{title}</h2>}
+    <section class={css.statistics}>
+      {title && <h2 class={css.title}>{title}</h2>}
 
-      <ul class="stat-list">
+      <ul class={css[`stat-list`]}>
         {stats.map(({ id, label, percentage }) => (
-          <li class="item" key={id} color={getRandomColor()}>
+          <li
+            className={css.item}
+            key={id}
+            style={{ backgroundColor: getRandomColor() }}
+          >
             <span>{label}</span>
-            <span class="percentage">{percentage}%</span>
+            <span class={css.percentage}>{percentage}%</span>
           </li>
         ))}
       </ul>
